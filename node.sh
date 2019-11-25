ba7dfa8bae8c8bb6cc397fb402478ff059cc6923
Kele Zsombor <kelezsombor@gmail.com>
2019-11-25 10:36:32 +0100

    sudok

--- node.sh
+++ node.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
-yum install -y gcc-c++ make nc -y
-curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
-sleep 1
-yum install -y nodejs
+#!/bin/bash  
+sudo yum install -y gcc-c++ make nc -y
+sudo curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
+sudo yum install -y nodejs
 firewall-cmd --add-port=6543/tcp --permanent
 firewall-cmd --reload
-npm install supervisor -g
-supervisor index.js &
+sudo npm install supervisor -g
+sudo supervisor index.js &
 
 
 
