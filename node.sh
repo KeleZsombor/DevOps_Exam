d2048d430a398c8604ff8389f777cbbb804d4332
Kele Zsombor <kelezsombor@gmail.com>
2019-11-25 10:42:40 +0100

    sudo firewall

--- node.sh
+++ node.sh
@@ -2,8 +2,8 @@
 sudo yum install -y gcc-c++ make nc -y
 sudo curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
 sudo yum install -y nodejs
-firewall-cmd --add-port=6543/tcp --permanent
-firewall-cmd --reload
+sudo firewall-cmd --add-port=6543/tcp --permanent
+sudo firewall-cmd --reload
 sudo npm install supervisor -g
 sudo supervisor index.js &
 
